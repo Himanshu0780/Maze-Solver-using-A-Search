a164e73b09d6029afa9d82d42c3ab2c64a271080
Himanshu <146204637+Himanshu0780@users.noreply.github.com>
2026-01-06 15:26:42 +0530

    Add files via upload

---
+++ MazeSolver.py
@@ -0,0 +1,67 @@
+from pyamaze import maze, agent, COLOR, textLabel
+from queue import PriorityQueue
+
+# Heuristic function: Manhattan distance between the current cell and the goal cell:(1,1)
+def h(cell):
+    x, y = cell
+    return (x - 1) + (y - 1)
+
+def aStar(m):
+    # Start cell is the bottom-right corner of the maze
+    start = (m.rows, m.cols)
+
+    # Initialize g_score and f_score for each cell in the maze
+    g_score = {cell:float('inf') for cell in m.grid}
+    g_score[start] = 0
+    f_score = {cell:float('inf') for cell in m.grid}
+    f_score[start] = h(start)
+
+    # Priority queue to store open cells, ordered by f_score
+    open = PriorityQueue()
+    open.put((f_score[start], h(start), start))
+    aPath = {}
+
+    while not open.empty():
+        currCell = open.get()[2]
+        if currCell == (1,1):
+            break
+
+        # Check adjacent cells
+        for d in "ESNW":
+            if m.maze_map[currCell][d] == True:
+                if d == 'E':
+                    childCell = (currCell[0], currCell[1] + 1)
+                elif d == 'W':
+                    childCell = (currCell[0], currCell[1] - 1)
+                elif d == 'N':
+                    childCell = (currCell[0] - 1, currCell[1])
+                elif d == 'S':
+                    childCell = (currCell[0] + 1, currCell[1])
+        
+                temp_g_score = g_score[currCell] + 1
+                temp_f_score = temp_g_score + h(childCell)
+
+                # Check if the new path of the adjacent cell is better
+                if temp_f_score < f_score[childCell]:
+                    g_score[childCell] = temp_g_score
+                    f_score[childCell] = temp_f_score
+                    open.put((temp_f_score, h(childCell), childCell))
+                    aPath[childCell] = currCell
+    
+    # Reconstruct the forward path from start to target
+    fwdPath = {}
+    cell = (1,1)
+    while cell != start:
+        fwdPath[aPath[cell]] = cell
+        cell = aPath[cell]
+
+    return fwdPath
+
+m = maze(100,10)   #Maze size can be adjusted from here
+m.CreateMaze()
+path = aStar(m)
+a = agent(m, footprints=True, color=COLOR.blue, filled=True)
+m.tracePath({a:path})
+l = textLabel(m, "A* path length", len(path)+1)
+
+m.run()
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# Maze-Solver-using-A-Search
-A Python-based AI project using Pyamaze that enables maze creation and real-time visualization of solving algorithms.
+# Maze solver (using A* algorithm)
+
+This project aims to solve mazes using the A* algorithm. The A* algorithm is a popular pathfinding algorithm widely used in robotics and video games due to its efficiency and ability to find the shortest path between two points in a maze.
+
+# Features
+- Generates random mazes of customizable dimensions using the `pyamaze` library.
+- Implements the A* algorithm to find the shortest path from the start to the target cell in the maze.
+- Visualizes the maze solving process using graphical representations.
+- Provides metrics such as the length of the A* path to evaluate the performance of the algorithm.
+
+# Getting Started
+To use this project, make sure you have Python installed on your system. You can install the required library using pip:
+`pip install pyamaze`
